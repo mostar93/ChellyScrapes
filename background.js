5f36b481d05a1544bd2e31fbe53c33d53c3634a3
Momin Ahmad <mominahmad93@gmail.com>
2020-06-30 12:35:43 -0500

    added download button

--- background.js
+++ background.js
@@ -9,10 +9,16 @@ chrome.runtime.onMessage.addListener(function(response, sender, senderResponse) 
     function getCSVLinkElement(arr){
 
         var link = document.createElement("a");
-        link.textContent = "Save as CSV";
+        // link.textContent = "Save as CSV";
+        var image = document.createElement("img")
+        image.setAttribute("src", "./download-button.svg");
+        image.setAttribute("height", "30");
+        image.setAttribute("width", "30");
         link.download = "file.csv";
         var csv = arr
         link.href = encodeURI("data:text/csv,"+csv);
+        link.appendChild(image)
+      
     
         return link;
     
